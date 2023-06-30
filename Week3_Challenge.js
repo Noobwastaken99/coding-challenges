694dffb62de12f3050ed063223af7326a2f77091
Noobwastaken99 <125329223+Noobwastaken99@users.noreply.github.com>
2023-06-29 21:34:03 -0500

    Update Week3_Challenge.js
    Have the function LongestWord(sen) take the sen parameter being passed and return the longest word in the string. If there are two or more words that are the same length, return the first word from the string with that length. Ignore punctuation and assume sen will not be empty. Words may also contain numbers, for example "Hello world123 567"

--- Week3_Challenge.js
+++ Week3_Challenge.js
@@ -1,1 +1,15 @@
+function LongestWord(sen) { 
+  sen = sen.replace(/[^a-z-A-Z ]/g, "").split(" ");
+  let big = "";
+  for (i=0; i < sen.length; i++) {
+    if (sen[i].length > big.length) {
+      big = sen[i];
+    }
+  }
+  // code goes here  
+  return sen = big; 
 
+}
+   
+// keep this function call here 
+console.log(LongestWord(readline()));
